8856be2c8f8f2a4e380621bc55a2a5200ee9f891
rudiansyah <rudiansyahether@gmail.com>
2024-02-20 22:11:03 +0700

    🔧 chore(database_config.ts): update database configuration to use a different host value for improved clarity and consistency
    🔒 chore(database_config.ts): comment out unused import statement to improve code readability

--- src/database/database_config.ts
+++ src/database/database_config.ts
@@ -1,11 +1,13 @@
 import { Sequelize } from 'sequelize';
+// import { TaskMode } from '../models/taskModel';
 
-const sequelize = new Sequelize({
+const sequelize = new Sequelize(
+  'testdb', 'user', 'password',
+  {
   dialect: 'sqlite',
-  database: 'myapp.sqlite',
+  // database: './myapp.sqlite',
+  host: './myapp.sqlite',
   logging: false,
-
-
 });
 
 
