602d5e3e41f35cd51b5d20a86ee6ab71f09ecf25
rudiansyah <rudiansyahether@gmail.com>
2024-02-22 10:05:18 +0700

    📦 chore(package.json): update dependencies and devDependencies
    🔀 chore(taskRouter.ts): add update route to taskRouter
    
    ℹ️ The package.json file was updated to include new dependencies and devDependencies. This ensures that the project has the necessary packages installed and up to date.
    
    🔀 The taskRouter.ts file was modified to include a new route for updating a task. This allows users to update existing tasks in the application.

--- src/routers/taskRouter.ts
+++ src/routers/taskRouter.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { index, show, store } from '../controllers/taskController';
+import { index, show, store, update } from '../controllers/taskController';
 
 const router = Router();
 router.get('/tasks', index);
 router.post('/tasks', store);
+router.post('/tasks/:id', update);
 router.get('/tasks/:id', show);
 
 export default router;
